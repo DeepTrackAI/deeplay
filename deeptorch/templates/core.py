56be3b1968cef350de56c027b634ea0a4311b553
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-08-14 04:18:16 +0200

    allow arithamtic between nodes

--- deeptorch/templates/core.py
+++ deeptorch/templates/core.py
@@ -11,13 +11,25 @@ class Node:
     def __rshift__(self, other):
         return NodeSequence(self, other)
     
-    def build(self, config):
+    def __add__(self, other):
+        return NodeAdd(self, other)
+    
+    def __sub__(self, other):
+        return NodeSub(self, other)
+    
+    def __mul__(self, other):
+        return NodeMul(self, other)
+    
+    def __div__(self, other):
+        return NodeDiv(self, other)
+    
+    def build(self, config: Config):
         subconfig = config.with_selector(self.className)
 
-        module = subconfig.get("module")
+        module = subconfig.get_module()
 
         if module is None:
-            raise ValueError(f"Module not found for selector {self.className} in config {subconfig._rules} with selector {subconfig._selector}")
+            raise ValueError(f"Module not found for selector {self.className} in config {subconfig._rules} with selector {subconfig._context}")
 
         if hasattr(module, "from_config"):
             return module.from_config(subconfig)
@@ -36,10 +48,104 @@ class NodeSequence(Node):
         self.nodes = nodes
 
     def __rshift__(self, other):
-        return NodeSequence(*self.layers, other)
+        return NodeSequence(*self.nodes, other)
 
     def build(self, config):
-        return nn.Sequential(*[node.build(config) for node in self.nodes])
+        
+        return Template(
+            **{node.className: node.build(config) for node in self.nodes}
+        )
+
+
+class NodeAdd(Node):
+
+    def __init__(self, a, b, **kwargs):
+        super().__init__(**kwargs)
+        self.a = a
+        self.b = b
+
+    def build(self, config):
+        return _TorchAdd(self.a.build(config), self.b.build(config))
+
+
+class NodeSub(Node):
+
+    def __init__(self, a, b, **kwargs):
+        super().__init__(**kwargs)
+        self.a = a
+        self.b = b
+
+    def build(self, config):
+        return _TorchSub(self.a.build(config), self.b.build(config))
+
+
+class NodeMul(Node):
+
+    def __init__(self, a, b, **kwargs):
+        super().__init__(**kwargs)
+        self.a = a
+        self.b = b
+
+    def build(self, config):
+        return _TorchMul(self.a.build(config), self.b.build(config))
+
+class NodeDiv(Node):
+
+    def __init__(self, a, b, **kwargs):
+        super().__init__(**kwargs)
+        self.a = a
+        self.b = b
+
+    def build(self, config):
+        return _TorchDiv(self.a.build(config), self.b.build(config))
+
+class Template(nn.ModuleDict):
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        
+    def forward(self, x):
+        for key, module in self.items():
+            x = module(x)
+        return x
+
+# == Helper classes for arithmetic operations
+class _TorchSub(nn.Module):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+    
+    def forward(self, x):
+        return self.a(x) - self.b(x)
+
+class _TorchAdd(nn.Module):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+    
+    def forward(self, x):
+        return self.a(x) + self.b(x)
+
+class _TorchMul(nn.Module):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+    
+    def forward(self, x):
+        return self.a(x) * self.b(x)
+
+
+class _TorchDiv(nn.Module):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+    
+    def forward(self, x):
+        return self.a(x) / self.b(x)
 
 
 # # Should be moved to components
