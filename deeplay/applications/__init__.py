7450246fa84437f4e545e5d0646377ff18b34099
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-11 09:46:00 +0100

    remove old code

--- deeplay/applications/__init__.py
+++
@@ -1,9 +0,0 @@
-from .application import Application
-from .classification import *
-from .regression import *
-
-# from .classification import *
-# from .autoencoders import *
-# from .segmentation import ImageSegmentor
-# from .gans import *
-# from .regression import *
