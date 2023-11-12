32aa8b942d0e333f4317206331c0f5279577ceb6
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-12 03:11:28 +0100

    Add applications module to deeplay package

--- deeplay/__init__.py
+++ deeplay/__init__.py
@@ -7,9 +7,11 @@ from torch.utils.data import DataLoader
 
 # Filter out warnings from lazy torch modules
 warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.modules.lazy")
+
 from .meta import ExtendedConstructorMeta
 from .module import DeeplayModule
 from .list import LayerList, Sequential
 from .external import *
 from .blocks import *
 from .components import *
+from .applications import *
