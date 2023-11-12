0fd1c52fe505b43095d8ec0af3298de8407f78b6
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-12 03:11:45 +0100

    Remove old application code

--- ____old/applications/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .applications import Application
-# from .classification import *
-# from .autoencoders import *
-# from .segmentation import ImageSegmentor
-# from .gans import *
-# from .regression import *
