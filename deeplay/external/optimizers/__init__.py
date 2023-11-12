760ade40053842597b19ed748fb0adebd954fd32
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-12 03:10:44 +0100

    Add optimizer classes: Adam, SGD, and RMSprop.

---
+++ deeplay/external/optimizers/__init__.py
@@ -0,0 +1,4 @@
+from .optimizer import Optimizer
+from .adam import Adam
+from .sgd import SGD
+from .rmsprop import RMSprop
---
+++ deeplay/external/optimizers/rmsprop.py
@@ -0,0 +1,31 @@
+from typing import Callable, Dict, List, Tuple, Union, Iterable
+
+from ..external import External
+from .optimizer import Optimizer
+
+import torch
+
+torch.optim.Adam
+
+
+class RMSprop(Optimizer):
+    def __pre_init__(self, **optimzer_kwargs):
+        super().__pre_init__(torch.optim.RMSprop, **optimzer_kwargs)
+
+    def __init__(self, **optimzer_kwargs):
+        super().__init__(**optimzer_kwargs)
+
+    # def __init__(self, **optimzer_kwargs):
+    #     super().__init__(**optimzer_kwargs)
+
+    def params(
+        self,
+        func: Callable[
+            [],
+            Iterable[torch.nn.Parameter]
+            | Dict[str, Iterable[torch.nn.Parameter]]
+            | List[Dict[str, Iterable[torch.nn.Parameter]]],
+        ],
+    ):
+        self.configure(params=func)
+        return self
