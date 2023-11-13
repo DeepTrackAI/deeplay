da2764b71e05dc1569eb1bd9ebabd6d55c44a2ab
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-13 14:57:49 +0100

    Clarify documentation

--- deeplay/components/mlp.py
+++ deeplay/components/mlp.py
@@ -13,20 +13,20 @@ class MultiLayerPerceptron(DeeplayModule):
     Configurables
     -------------
 
-    - in_features (int): Number of input features. If None, the input shape is inferred from the first forward pass. (Default: None)
-    - hidden_dims (list[int]): Number of hidden units in each layer. (Default: [32, 32])
+    - in_features (int): Number of input features. If None, the input shape is inferred in the first forward pass. (Default: None)
+    - hidden_dims (list[int]): Number of hidden units in each layer.
     - out_features (int): Number of output features. (Default: 1)
     - blocks (template-like): Specification for the blocks of the MLP. (Default: "layer" >> "activation" >> "normalization" >> "dropout")
         - layer (template-like): Specification for the layer of the block. (Default: nn.Linear)
         - activation (template-like): Specification for the activation of the block. (Default: nn.ReLU)
         - normalization (template-like): Specification for the normalization of the block. (Default: nn.Identity)
-        - dropout (template-like): Specification for the dropout of the block. (Default: nn.Identity)
     - out_activation (template-like): Specification for the output activation of the MLP. (Default: nn.Identity)
 
-    Constraints
-    -----------
-    - input shape: (batch_size, ch_in)
-    - output shape: (batch_size, ch_out)
+    Shorthands
+    ----------
+    - `input`: Equivalent to `.blocks[0]`.
+    - `hidden`: Equivalent to `.blocks[:-1]`.
+    - `output`: Equivalent to `.blocks[-1]`.
 
     Evaluation
     ----------
@@ -36,27 +36,16 @@ class MultiLayerPerceptron(DeeplayModule):
 
     Examples
     --------
-    >>> # Using default values
-    >>> mlp = MultiLayerPerceptron(28 * 28, [128], 10)
-    >>> # Customizing output activation
-    >>> mlp = MultiLayerPerceptron(28 * 28, [128], 1, nn.Sigmoid)
-    >>> # Using from_config with custom normalization
-    >>> mlp = MultiLayerPerceptron.from_config(
-    >>>     Config()
-    >>>     .in_features(28 * 28)
-    >>>     .hidden_dims([128])
-    >>>     .out_features(1)
-    >>>     .out_activation(nn.Sigmoid)
-    >>>     .blocks[0].normalization(nn.BatchNorm1d, num_features=128)
-    >>> )
+    >>> mlp = MultiLayerPerceptron(28 * 28, [128, 128], 10)
+    >>> mlp.input.layer.configure(kernel_size=5)
+    >>> mlp.hidden.normalization.configure(nn.BatchNorm1d)
+    >>> mlp.output.activation.configure(nn.Softmax)
+    >>> mlp.build()
+
 
     Return Values
     -------------
     The forward method returns the processed tensor.
-
-    Additional Notes
-    ----------------
-    The `Config` and `Layer` classes are used for configuring the blocks of the MLP. For more details refer to [Config Documentation](#) and [Layer Documentation](#).
 
     """
 
@@ -64,21 +53,20 @@ class MultiLayerPerceptron(DeeplayModule):
     hidden_dims: Sequence[Optional[int]]
     out_features: int
     blocks: LayerList[LayerActivationNormalizationBlock]
-    out_layer: LayerActivationNormalizationBlock
 
     @property
     def input(self):
-        """Return the input layer of the network."""
+        """Return the input layer of the network. Equivalent to `.blocks[0]`."""
         return self.blocks[0]
 
     @property
     def hidden(self):
-        """Return the hidden layers of the network (inclusive first, exclusive last)."""
+        """Return the hidden layers of the network. Equivalent to `.blocks[:-1]`"""
         return self.blocks[:-1]
 
     @property
     def output(self):
-        """Return the last layer of the network."""
+        """Return the last layer of the network. Equivalent to `.blocks[-1]`."""
         return self.blocks[-1]
 
     def __init__(
@@ -135,21 +123,9 @@ class MultiLayerPerceptron(DeeplayModule):
         self,
         /,
         in_features: int | None = None,
-        hidden_dims: List[int] | None = None,
+        hidden_features: List[int] | None = None,
         out_features: int | None = None,
         out_activation: Type[nn.Module] | nn.Module | None = None,
-    ) -> None:
-        ...
-
-    @overload
-    def configure(
-        self,
-        name: Literal["out_layer", "blocks"],
-        order: Optional[Sequence[str]] = None,
-        layer: Optional[Type[nn.Module]] = None,
-        activation: Optional[Type[nn.Module]] = None,
-        normalization: Optional[Type[nn.Module]] = None,
-        **kwargs: Any,
     ) -> None:
         ...
 
