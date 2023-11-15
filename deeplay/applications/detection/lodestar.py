4adab002c9205691d37518483b2f8716021cf275
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-11-15 09:04:34 +0100

    Refactor consistency loss and add detection methods

--- deeplay/applications/detection/lodestar.py
+++ deeplay/applications/detection/lodestar.py
@@ -4,24 +4,23 @@ from ...components import ConvolutionalNeuralNetwork
 import torch
 import torch.nn as nn
 
+import numpy as np
+
+
+from skimage import morphology
+import scipy.ndimage
+import scipy
+
+
 
 class LodeSTAR(Application):
     def __init__(
-        self,
-        model=None,
-        num_outputs=2,
-        intra_consistency_loss=None,
-        inter_consistency_loss=None,
-        **kwargs
+        self, model=None, num_outputs=2, between_loss=None, within_loss=None, **kwargs
     ):
         self.num_outputs = num_outputs
         self.model = model or self._build_default_model()
-        self.intra_consistency_loss = intra_consistency_loss or nn.L1Loss(
-            reduction="mean"
-        )
-        self.inter_consistency_loss = inter_consistency_loss or nn.L1Loss(
-            reduction="mean"
-        )
+        self.between_loss = between_loss or nn.L1Loss(reduction="mean")
+        self.within_loss = within_loss or nn.L1Loss(reduction="mean")
 
         super().__init__(**kwargs)
 
@@ -33,8 +32,10 @@ class LodeSTAR(Application):
         )
         cnn.blocks[2].pool.configure(nn.MaxPool2d, kernel_size=2)
 
+        return cnn
+
     def forward(self, x):
-        B, C, Hx, Wx = x.shape
+        _, _, Hx, Wx = x.shape
 
         y = self.model(x)
 
@@ -68,24 +69,98 @@ class LodeSTAR(Application):
         y_reduced = self.reduce(y_pred, weights)
 
         consistency = (y_pred - y_reduced[..., None, None]) * weights
-        consistency_loss = self.intra_consistency_loss(
-            consistency, torch.zeros_like(consistency)
-        )
+        consistency_loss = self.within_loss(consistency, torch.zeros_like(consistency))
 
         y_reduced_on_initial = self.apply_inverse(y_reduced, y)
 
         average_on_initial = y_reduced_on_initial.mean(dim=0, keepdim=True)
 
-        inter_consistency_loss = self.inter_consistency_loss(
+        inter_consistency_loss = self.between_loss(
             y_reduced_on_initial, average_on_initial
         )
 
         return {
-            "consistency_loss": consistency_loss,
-            "inter_consistency_loss": inter_consistency_loss,
+            "between_image_disagreement": consistency_loss,
+            "within_image_disagreement": inter_consistency_loss,
         }
 
     def apply_inverse(self, y_reduced, y):
         for f in reversed(y):
             y_reduced = f(y_reduced)
         return y_reduced
+
+    def detect(self, x, alpha=0.5, beta=.5, cutoff=0.97, mode="quantile")
+        y = self.model(x.to(self.device))
+        y_pred, weights = y[:, :-1], y[:, -1]
+        detections = [
+            self.detect_single(y_pred[i], weights[i], alpha, beta, cutoff, mode)
+            for i in range(len(y_pred))
+        ]
+
+        return detections
+    
+    def pooled(self, x, mask=1):
+        y = self.model(x.to(self.device))
+        y_pred, weights = y[:, :-1], y[:, -1]
+        masked
+
+        return pooled
+    
+    def detect_single(self, y_pred, weights, alpha=0.5, beta=.5, cutoff=0.97, mode="quantile"):
+        score = self.get_detection_score(y_pred, weights, alpha, beta)
+        return self.find_local_maxima(y_pred, score, cutoff, mode)
+
+    @staticmethod
+    def find_local_maxima(pred, score, cutoff=0.9, mode="quantile"):
+        """Finds the local maxima in a score-map, indicating detections
+
+        Parameters
+            ----------
+        pred, score: array-like
+            Output from model, score-map
+        cutoff, mode: float, string
+            Treshholding parameters. Mode can be either "quantile" or "ratio" or "constant". If "quantile", then
+            `ratio` defines the quantile of scores to accept. If "ratio", then cutoff defines the ratio of the max
+            score as threshhold. If constant, the cutoff is used directly as treshhold.
+
+        """
+        score = score[3:-3, 3:-3]
+        th = cutoff
+        if mode == "quantile":
+            th = np.quantile(score, cutoff)
+        elif mode == "ratio":
+            th = np.max(score.flatten()) * cutoff
+        hmax = morphology.h_maxima(np.squeeze(score), th) == 1
+        hmax = np.pad(hmax, ((3, 3), (3, 3)))
+        detections = pred[hmax, :]
+        return np.array(detections)
+
+    @staticmethod
+    def local_consistency(pred):
+        """Calculate the consistency metric
+
+        Parameters
+        ----------
+        pred : array-like
+            first output from model
+        """
+        pred = pred.permute(0, 2, 3, 1).cpu().detach().numpy()
+        kernel = np.ones((1, 3, 3)) / 3 ** 2
+        pred_local_squared = scipy.signal.convolve(pred, kernel, "same") ** 2
+        squared_pred_local = scipy.signal.convolve(pred ** 2, kernel, "same")
+        squared_diff = (squared_pred_local - pred_local_squared).sum(-1)
+        np.clip(squared_diff, 0, np.inf, squared_diff)
+        return 1 / (1e-6 + squared_diff)
+    
+    @classmethod
+    def get_detection_score(cls, pred, weights, alpha=0.5, beta=0.5):
+        """Calculates the detection score as weights^alpha * consistency ^ beta.
+
+        Parameters
+        ----------
+        pred, weights: array-like
+            Output from model
+        alpha, beta: float
+            Geometric weight of the weight-map vs the consistenct metric for detection.
+        """
+        return weights[..., 0] ** alpha * cls.local_consistency(pred) ** beta
