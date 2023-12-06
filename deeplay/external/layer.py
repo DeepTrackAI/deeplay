9b87f9558e4288ec7c88ab0ef703f697485e8d9a
Jesús Pineda <jesus.pineda@physics.gu.se>
2023-12-06 08:29:21 +0100

    implement _set_dict_mapping

--- deeplay/external/layer.py
+++ deeplay/external/layer.py
@@ -1,4 +1,4 @@
-from typing import Any, Type, overload, Dict
+from typing import Any, Type, overload, Dict, Union, List
 from .external import External
 from functools import partial
 
@@ -7,43 +7,86 @@ import torch.nn as nn
 from ..decorators import after_build
 
 
-def _create_forward_with_extras(old_forward, extra_arguments: Dict[str, str]):
-    def forward_with_extras(self, *args, extras={}, **kwargs):
-        assert all(
-            key not in kwargs for key in extra_arguments.keys()
-        ), f"Cannot pass {extra_arguments.keys()} as keyword arguments. Use extras instead."
+def _create_forward_with_input_dict(
+    old_forward,
+    input_arguments: List[str],
+    input_kwargs: Dict[str, str],
+    output_arguments: Dict[str, int],
+):
+    def forward_with_input_dict(self, x):
+        x = x.copy()
 
-        missing_extras = set(extra_arguments.values()) - set(extras.keys())
-        assert (
-            not missing_extras
-        ), f"module {self} did not receive the following extras: {missing_extras}"
+        outputs = old_forward(
+            self,
+            *[x.get(arg) for arg in input_arguments],
+            **{key: x.get(value) for key, value in input_kwargs.items()},
+        )
 
-        my_extras = {key: extras[value] for key, value in extra_arguments.items()}
-        return old_forward(self, *args, **my_extras)
+        if not isinstance(outputs, tuple):
+            outputs = (outputs,)
 
-    return forward_with_extras
+        expected_outputs = len(set(output_arguments.values()))
+        assert len(outputs) == expected_outputs, (
+            f"layer {self} returned {len(outputs)} outputs, "
+            f"but it should return {expected_outputs}"
+        )
+
+        x.update({key: outputs[value] for key, value in output_arguments.items()})
+        return x
+
+    return forward_with_input_dict
 
 
 class Layer(External):
     def __pre_init__(self, classtype: Type[nn.Module], *args, **kwargs):
         super().__pre_init__(classtype, *args, **kwargs)
 
+    def set_dict_input_mapping(self, *args: str, **kwargs: str):
+        self.__dict__.update(
+            {
+                "input_arguments": args,
+                "input_kwargs": kwargs,
+                "_dict_input_mapping_called": True,
+            }
+        )
+        self._check_and_execute_dict_mapping()
+
+    def set_dict_output_mapping(self, *args: str, **kwargs: int):
+        output_arguments = {arg: i for i, arg in enumerate(args)}
+        output_arguments.update(kwargs)
+
+        self.__dict__.update(
+            {
+                "output_arguments": output_arguments,
+                "_dict_output_mapping_called": True,
+            }
+        )
+        self._check_and_execute_dict_mapping()
+
+    def _check_and_execute_dict_mapping(self):
+        if getattr(self, "_dict_input_mapping_called", False) and getattr(
+            self, "_dict_output_mapping_called", False
+        ):
+            self.set_dict_mapping(
+                self.input_arguments, self.input_kwargs, self.output_arguments
+            )
+
     @after_build
-    def set_extras(layer: nn.Module, *args: str, **kwargs: str):
-        extras = {}
-
-        for arg in args:
-            extras[arg] = arg
-
-        extras.update(kwargs)
-
-        # monkey patch the forward method to include the extras
+    def set_dict_mapping(
+        layer: nn.Module,
+        input_arguments: List[str],
+        input_kwargs: Dict[str, str],
+        output_arguments: Dict[str, int],
+    ):
+        # monkey patch the forward method to include dict
         # using type(layer) to get the base implementation of forward.
-        # This is necessary so that multiple calls to set_extras don't
+        # This is necessary so that multiple calls to set_input_dict don't
         # chain the monkey patching.
         # We use partial to bind the instance to make it a method.
         layer.forward = partial(
-            _create_forward_with_extras(type(layer).forward, extras),
+            _create_forward_with_input_dict(
+                type(layer).forward, input_arguments, input_kwargs, output_arguments
+            ),
             layer,
         )
 
