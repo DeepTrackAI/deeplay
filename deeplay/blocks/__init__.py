2f55e61b6d05d015e10b861dfe37b6870a9654be
Henrik K M <ccx555@gmail.com>
2023-12-18 19:28:02 +0100

    Jp/rnns Added models package, with first model RNN, and LAND layers with custominterlayer dropout

--- deeplay/blocks/__init__.py
+++ deeplay/blocks/__init__.py
@@ -2,3 +2,4 @@ from .block import Block
 from .LA import LayerActivation
 from .LAN import LayerActivationNormalization
 from .PLAN import PoolLayerActivationNormalization
+from .LAND import LayerActivationNormalizationDropout
