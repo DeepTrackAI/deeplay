3fa0faf56d2ae61478edb0309c87c18be3c42ce2
Jesús Pineda <jesus.pineda@physics.gu.se>
2023-12-29 03:28:37 +0100

    update naming for clarity

--- deeplay/components/gnn/gnn.py
+++ deeplay/components/gnn/gnn.py
@@ -1,9 +1,9 @@
 from typing import List, Optional, Literal, Any, Sequence, Type, overload, Union
 
 from ... import DeeplayModule, Layer, LayerList, LayerActivationNormalization
-from .TAU import TransformAggregateUpdate
+from .TPU import TransformPropagateUpdate
 
-from .normalization import sparse_normalization
+from .normalization import sparse_laplacian_normalization
 
 import torch
 import torch.nn as nn
@@ -13,7 +13,7 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
     in_channels: Optional[int]
     hidden_channels: Sequence[Optional[int]]
     out_channels: int
-    blocks: LayerList[TransformAggregateUpdate]
+    blocks: LayerList[TransformPropagateUpdate]
 
     @property
     def input_block(self):
@@ -59,15 +59,15 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
         elif isinstance(out_activation, type) and issubclass(out_activation, nn.Module):
             out_activation = Layer(out_activation)
 
-        self.normalize = Layer(sparse_normalization)
+        self.normalize = Layer(sparse_laplacian_normalization)
         self.normalize.set_input_map("x", "A")
         self.normalize.set_output_map("A")
 
-        class aggregation(nn.Module):
+        class propagation(nn.Module):
             def forward(self, x, A):
                 if A.is_sparse:
                     return torch.spmm(A, x)
-                elif torch.is_tensor(A) & (A.size(0) == 2):
+                elif (not A.is_sparse) & (A.size(0) == 2):
                     A = torch.sparse_coo_tensor(
                         A,
                         torch.ones(A.size(1)),
@@ -75,13 +75,13 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
                         device=A.device,
                     )
                     return torch.spmm(A, x)
-                elif torch.is_tensor(A) & (A.size(0) == A.size(1)):
+                elif (not A.is_sparse) & len({A.size(0), A.size(1), x.size(0)}) == 1:
                     return A @ x
                 else:
                     raise ValueError(
                         "Unsupported adjacency matrix format.",
                         "Ensure it is a pytorch sparse tensor, an edge index tensor, or a square dense tensor.",
-                        "Consider updating the aggregate layer to handle alternative formats.",
+                        "Consider updating the propagate layer to handle alternative formats.",
                     )
 
         self.blocks = LayerList()
@@ -95,17 +95,17 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
             transform.set_input_map("x")
             transform.set_output_map("x")
 
-            aggregate = Layer(aggregation)
-            aggregate.set_input_map("x", "A")
-            aggregate.set_output_map("x")
+            propagate = Layer(propagation)
+            propagate.set_input_map("x", "A")
+            propagate.set_output_map("x")
 
             update = Layer(nn.ReLU) if i < len(self.hidden_channels) else out_activation
             update.set_input_map("x")
             update.set_output_map("x")
 
-            block = TransformAggregateUpdate(
+            block = TransformPropagateUpdate(
                 transform=transform,
-                aggregate=aggregate,
+                propagate=propagate,
                 update=update,
             )
             self.blocks.append(block)
@@ -133,7 +133,7 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
         name: Literal["blocks"],
         order: Optional[Sequence[str]] = None,
         transform: Optional[Type[nn.Module]] = None,
-        aggregate: Optional[Type[nn.Module]] = None,
+        propagate: Optional[Type[nn.Module]] = None,
         update: Optional[Type[nn.Module]] = None,
         **kwargs: Any,
     ) -> None:
@@ -146,7 +146,7 @@ class GraphConvolutionalNeuralNetwork(DeeplayModule):
         index: Union[int, slice, List[Union[int, slice]]],
         order: Optional[Sequence[str]] = None,
         transform: Optional[Type[nn.Module]] = None,
-        aggregate: Optional[Type[nn.Module]] = None,
+        propagate: Optional[Type[nn.Module]] = None,
         update: Optional[Type[nn.Module]] = None,
         **kwargs: Any,
     ) -> None:
