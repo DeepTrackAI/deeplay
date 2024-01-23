b6a7501295b42a00a3848d69b7628adb9c9b463b
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-01-23 16:36:07 +0100

    Track current root model

--- deeplay/meta.py
+++ deeplay/meta.py
@@ -4,7 +4,7 @@ T = TypeVar("T")
 
 
 class ExtendedConstructorMeta(type):
-    _is_top_level = {"value": True}
+    _is_top_level = {"value": True, "current_root_module": None}
 
     def __new__(cls, name, bases, attrs):
         return super().__new__(cls, name, bases, attrs)
@@ -21,21 +21,29 @@ class ExtendedConstructorMeta(type):
             obj._user_config = __user_config
 
         # if cls._is_top_level["value"]:
-        with not_top_level(cls):
+        with not_top_level(cls, obj):
             obj.__construct__()
             obj.__post_init__()
 
         return obj
 
 
-def not_top_level(cls: ExtendedConstructorMeta):
+def not_top_level(cls: ExtendedConstructorMeta, obj):
     current_value = cls._is_top_level["value"]
 
     class ContextManager:
+        def __init__(self, obj):
+            self.original_is_top_level = cls._is_top_level["value"]
+            self.original_current_root_module = cls._is_top_level["current_root_module"]
+            self.obj = obj
+
         def __enter__(self):
-            cls._is_top_level["value"] = False
+            if self.original_is_top_level:
+                cls._is_top_level["value"] = False
+                cls._is_top_level["current_root_module"] = self.obj
 
         def __exit__(self, *args):
-            cls._is_top_level["value"] = current_value
+            cls._is_top_level["value"] = self.original_is_top_level
+            cls._is_top_level["current_root_module"] = self.original_current_root_module
 
-    return ContextManager()
+    return ContextManager(obj)
