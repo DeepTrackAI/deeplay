00629188e45b47ea1bd5d7ce2a5dec75af844494
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-02-16 18:19:35 +0100

    Add find_packages() to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -10,15 +10,27 @@ setup(
     name="deeplay",
     version="0.0.6",
     license="MIT",
-    author=("Benjamin Midtvedt, Jesus Pineda, Henrik Klein Moberg, " 
-            "Harshith Bachimanchi, Carlo Manzo, Giovanni Volpe"),
-    description=("An AI-powered platform for advancing deep learning research "
-                 "and applications, developed by DeepTrackAI."),
+    packages=find_packages(),
+    author=(
+        "Benjamin Midtvedt, Jesus Pineda, Henrik Klein Moberg, "
+        "Harshith Bachimanchi, Carlo Manzo, Giovanni Volpe"
+    ),
+    description=(
+        "An AI-powered platform for advancing deep learning research "
+        "and applications, developed by DeepTrackAI."
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DeepTrackAI/deeplay",
-    keywords=["AI", "Deep Learning", "Machine Learning", "Data Science", 
-              "Research Platform", "Artificial Intelligence", "Technology"],
-    python_requires='>=3.8',
+    keywords=[
+        "AI",
+        "Deep Learning",
+        "Machine Learning",
+        "Data Science",
+        "Research Platform",
+        "Artificial Intelligence",
+        "Technology",
+    ],
+    python_requires=">=3.8",
     install_requires=required,
 )
