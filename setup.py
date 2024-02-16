01c0817080d66f2b4444ff31a5937e610e6a0c55
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-02-17 01:13:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 with open("requirements.txt", "r") as file:
     required = file.read().splitlines()
 
 setup(
     name="deeplay",
-    version="0.0.5",
+    version="0.0.6",
     license="MIT",
-    author="Benjamin Midtvedt",
-    author_email="email@example.com",
-    packages=find_packages(),
-    url="https://github.com/softmatterlab/deeplay",
-    keywords="example project",
+    author=("Benjamin Midtvedt, Jesus Pineda, Henrik Klein Moberg, " 
+            "Harshith Bachimanchi, Carlo Manzo, Giovanni Volpe"),
+    description=("An AI-powered platform for advancing deep learning research "
+                 "and applications, developed by DeepTrackAI."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DeepTrackAI/deeplay",
+    keywords=["AI", "Deep Learning", "Machine Learning", "Data Science", 
+              "Research Platform", "Artificial Intelligence", "Technology"],
+    python_requires='>=3.8',
     install_requires=required,
 )
