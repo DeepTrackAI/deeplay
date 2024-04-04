178f45bf211575c977848fa8de283bac63f15204
Jesús Pineda <jesus.pineda@physics.gu.se>
2024-04-04 14:22:10 +0200

    Update test_gnn.py

--- deeplay/tests/test_gnn.py
+++ deeplay/tests/test_gnn.py
@@ -32,7 +32,7 @@ class TestComponentGCN(unittest.TestCase):
         # test on a batch of 2
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 1, 2, 1], [1, 0, 2, 1, 0]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2, 1], [1, 0, 2, 1, 0]])
         out = gnn(inp)
         self.assertEqual(out["x"].shape, (3, 1))
 
@@ -50,7 +50,7 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
 
         out = gnn(inp)
         self.assertTrue(
@@ -63,7 +63,7 @@ class TestComponentGCN(unittest.TestCase):
                         [0.0000, 0.4082, 0.5000],
                     ]
                 )
-                - out["A"].to_dense()
+                - out["edge_index"].to_dense()
             ).sum()
             < 1e-4,
         )
@@ -76,10 +76,10 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
 
         out = gnn(inp)
-        self.assertTrue(torch.all(inp["A"] == out["A"].to_dense()))
+        self.assertTrue(torch.all(inp["edge_index"] == out["edge_index"].to_dense()))
         self.assertEqual(out["x"].shape, (3, 1))
 
     def test_normalization_with_sparse_A_and_repd_edges(self):
@@ -90,7 +90,7 @@ class TestComponentGCN(unittest.TestCase):
         inp = {}
         inp["x"] = torch.randn(3, 2)
         # edge (2, 1) is repeated
-        inp["A"] = torch.tensor([[0, 1, 1, 2, 2], [1, 0, 2, 1, 1]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2, 2], [1, 0, 2, 1, 1]])
 
         out = gnn(inp)
         self.assertTrue(
@@ -103,7 +103,7 @@ class TestComponentGCN(unittest.TestCase):
                         [0.0000, 0.4082, 0.5000],
                     ]
                 )
-                - out["A"].to_dense()
+                - out["edge_index"].to_dense()
             ).sum()
             < 1e-4,
         )
@@ -116,7 +116,7 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+        inp["edge_index"] = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
         out = gnn(inp)
         self.assertTrue(
@@ -129,7 +129,7 @@ class TestComponentGCN(unittest.TestCase):
                         [0.0000, 0.4082, 0.5000],
                     ]
                 )
-                - out["A"].to_dense()
+                - out["edge_index"].to_dense()
             ).sum()
             < 1e-4,
         )
@@ -142,10 +142,10 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+        inp["edge_index"] = torch.tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
         out = gnn(inp)
-        self.assertTrue(torch.all(inp["A"] == out["A"].to_dense()))
+        self.assertTrue(torch.all(inp["edge_index"] == out["edge_index"].to_dense()))
         self.assertEqual(out["x"].shape, (3, 1))
 
     def test_numeric_output(self):
@@ -156,7 +156,7 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
 
         out = gnn(inp)
         self.assertTrue(torch.is_tensor(out))
@@ -173,7 +173,7 @@ class TestComponentGCN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(3, 2)
-        inp["A"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+        inp["edge_index"] = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
 
         out = gnn(inp)
         self.assertTrue(torch.all(out["x"] == 0))
@@ -194,14 +194,14 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 30))
-        inp["edgefeat"] = torch.ones(30, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 30))
+        inp["edge_attr"] = torch.ones(30, 1)
 
         out = gnn(inp)
 
         self.assertEqual(out["x"].shape, (10, 1))
-        self.assertEqual(out["edgefeat"].shape, (30, 1))
-        self.assertTrue(torch.all(inp["A"] == out["A"]))
+        self.assertEqual(out["edge_attr"].shape, (30, 1))
+        self.assertTrue(torch.all(inp["edge_index"] == out["edge_index"]))
 
     def test_gnn_change_depth(self):
         gnn = MessagePassingNeuralNetwork([4], 1)
@@ -215,7 +215,7 @@ class TestComponentMPN(unittest.TestCase):
         gnn.configure(out_activation=nn.Sigmoid)
         gnn.create()
         gnn.build()
-        
+
         self.assertIsInstance(gnn.output.transform.activation, nn.Sigmoid)
         self.assertIsInstance(gnn.output.update.activation, nn.Sigmoid)
 
@@ -226,14 +226,14 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 30))
-        inp["edgefeat"] = torch.ones(30, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 30))
+        inp["edge_attr"] = torch.ones(30, 1)
 
         # by default, the propagation is a sum
         propagator = gnn.propagate[0]
         out = propagator(inp)
 
-        uniques = torch.unique(inp["A"][1], return_counts=True)
+        uniques = torch.unique(inp["edge_index"][1], return_counts=True)
         expected = torch.zeros(10, 1)
         expected[uniques[0]] = uniques[1].unsqueeze(1).float()
 
@@ -243,7 +243,7 @@ class TestComponentMPN(unittest.TestCase):
         gnn = MessagePassingNeuralNetwork([4], 1)
 
         gnn.blocks[0].replace("propagate", Mean())
-        gnn.blocks.propagate.set_input_map("x", "A", "edgefeat")
+        gnn.blocks.propagate.set_input_map("x", "edge_index", "edge_attr")
         gnn.blocks.propagate.set_output_map("aggregate")
 
         gnn.create()
@@ -251,13 +251,13 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 3))
-        inp["edgefeat"] = torch.ones(3, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 3))
+        inp["edge_attr"] = torch.ones(3, 1)
 
         propagator = gnn.propagate[0]
         out = propagator(inp)
 
-        uniques = torch.unique(inp["A"][1])
+        uniques = torch.unique(inp["edge_index"][1])
         expected = torch.zeros(10, 1)
         expected[uniques] = 1.0
 
@@ -267,7 +267,7 @@ class TestComponentMPN(unittest.TestCase):
         gnn = MessagePassingNeuralNetwork([4], 1)
 
         gnn.blocks[0].replace("propagate", Prod())
-        gnn.blocks.propagate.set_input_map("x", "A", "edgefeat")
+        gnn.blocks.propagate.set_input_map("x", "edge_index", "edge_attr")
         gnn.blocks.propagate.set_output_map("aggregate")
 
         gnn.create()
@@ -275,13 +275,13 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 20))
-        inp["edgefeat"] = torch.ones(20, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 20))
+        inp["edge_attr"] = torch.ones(20, 1)
 
         propagator = gnn.propagate[0]
         out = propagator(inp)
 
-        uniques = torch.unique(inp["A"][1])
+        uniques = torch.unique(inp["edge_index"][1])
         expected = torch.zeros(10, 1)
         expected[uniques] = 1.0
 
@@ -291,7 +291,7 @@ class TestComponentMPN(unittest.TestCase):
         gnn = MessagePassingNeuralNetwork([4], 1)
 
         gnn.blocks[0].replace("propagate", Min())
-        gnn.blocks.propagate.set_input_map("x", "A", "edgefeat")
+        gnn.blocks.propagate.set_input_map("x", "edge_index", "edge_attr")
         gnn.blocks.propagate.set_output_map("aggregate")
 
         gnn.create()
@@ -299,13 +299,13 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 20))
-        inp["edgefeat"] = torch.ones(20, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 20))
+        inp["edge_attr"] = torch.ones(20, 1)
 
         propagator = gnn.propagate[0]
         out = propagator(inp)
 
-        uniques = torch.unique(inp["A"][1])
+        uniques = torch.unique(inp["edge_index"][1])
         expected = torch.zeros(10, 1)
         expected[uniques] = 1.0
 
@@ -315,7 +315,7 @@ class TestComponentMPN(unittest.TestCase):
         gnn = MessagePassingNeuralNetwork([4], 1)
 
         gnn.blocks[0].replace("propagate", Max())
-        gnn.blocks.propagate.set_input_map("x", "A", "edgefeat")
+        gnn.blocks.propagate.set_input_map("x", "edge_index", "edge_attr")
         gnn.blocks.propagate.set_output_map("aggregate")
 
         gnn.create()
@@ -323,13 +323,13 @@ class TestComponentMPN(unittest.TestCase):
 
         inp = {}
         inp["x"] = torch.randn(10, 2)
-        inp["A"] = torch.randint(0, 10, (2, 20))
-        inp["edgefeat"] = torch.ones(20, 1)
+        inp["edge_index"] = torch.randint(0, 10, (2, 20))
+        inp["edge_attr"] = torch.ones(20, 1)
 
         propagator = gnn.propagate[0]
         out = propagator(inp)
 
-        uniques = torch.unique(inp["A"][1])
+        uniques = torch.unique(inp["edge_index"][1])
         expected = torch.zeros(10, 1)
         expected[uniques] = 1.0
 
