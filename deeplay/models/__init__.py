2bffd619c13ade7720d5e3bee778c4182c76572b
Jesús Pineda <jesus.pineda@physics.gu.se>
2024-04-04 18:31:58 +0200

    Update __init__.py

--- deeplay/models/__init__.py
+++ deeplay/models/__init__.py
@@ -1,2 +1,6 @@
 from .recurrentmodel import RecurrentModel
+from .mpm import MPM
+from .gtogmpm import GraphToGlobalMPM, GlobalMeanPool
+from .gtonmpm import GraphToNodeMPM
+from .gtoempm import GraphToEdgeMPM
 from .backbones import *
