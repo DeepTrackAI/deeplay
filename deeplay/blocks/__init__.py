206b542e1f751108432328a03cbbf3399b572140
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-04-07 22:01:51 +0200

    Add Conv2dBlock and LinearBlock to deeplay/blocks/__init__.py

--- deeplay/blocks/__init__.py
+++ deeplay/blocks/__init__.py
@@ -6,4 +6,7 @@ from .land import LayerActivationNormalizationDropout
 from .recurrentblock import RecurrentBlock
 from .lanu import LayerActivationNormalizationUpsample
 from .residual import BaseResidual, Conv2dResidual
-from .conv2d import *
+from .conv2d import Conv2dBlock
+from .linear import LinearBlock
+
+# from .conv2d import
