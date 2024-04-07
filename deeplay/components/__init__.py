3fbee51432d133fbadc4aaa3f1264ed68fd1d961
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-04-07 22:02:37 +0200

    Commented out DCGAN import

--- deeplay/components/__init__.py
+++ deeplay/components/__init__.py
@@ -1,7 +1,8 @@
 from .mlp import MultiLayerPerceptron
 from .cnn import *
 from .rnn import *
-from .dcgan import *
+
+# from .dcgan import *
 from .gnn import *
 from .dict import *
 from .transformer import *
