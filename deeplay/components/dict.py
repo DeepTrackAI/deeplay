764f3d66b19ef7e083ae628e932b24dc9f72047a
Jesús Pineda <jesus.pineda@physics.gu.se>
2024-04-08 11:15:13 +0200

    Create AddDict component

--- deeplay/components/dict.py
+++ deeplay/components/dict.py
@@ -2,6 +2,9 @@ from typing import Dict, Any, Union, Tuple, overload
 
 from deeplay import DeeplayModule
 
+import torch
+from torch_geometric.data import Data
+
 
 class FromDict(DeeplayModule):
     def __init__(self, *keys: str):
@@ -17,3 +20,37 @@ class FromDict(DeeplayModule):
 
     def extra_repr(self) -> str:
         return ", ".join(self.keys)
+
+
+class AddDict(DeeplayModule):
+    """
+    Element-wise addition of two dictionaries.
+
+    Parameters
+    ----------
+    keys : Tuple[str]
+        Specifies the keys to be added element-wise.
+
+    Constraints
+    -----------
+    - Both dictionaries 'x' (base) and 'y' (addition) must contain the same keys for the addition operation.
+
+    - 'x': Dict[str, Any] or torch_geometric.data.Data.
+    - 'y': Dict[str, Any] or torch_geometric.data.Data.
+    """
+
+    def __init__(self, *keys: str):
+        super().__init__()
+        self.keys = keys
+
+    def forward(
+        self, x: Union[Dict[str, Any], Data], y: Dict[str, Any]
+    ) -> Union[Dict[str, Any], Data]:
+
+        if isinstance(x, Data):
+            x = x.clone()
+        else:
+            x = x.copy()
+
+        x.update({key: torch.add(x[key], y[key]) for key in self.keys})
+        return x
