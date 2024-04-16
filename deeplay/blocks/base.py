fix configuration of normalization

deeplay/blocks/base.py
@@ -15,25 +15,30 @@ from deeplay.ops.merge import MergeOp
 
 class DeferredConfigurableLayer:
 
-    def __init__(self, parent: SequentialBlock, name: str, after: str):
+    def __init__(self, parent: SequentialBlock, name: str):
         self.parent = parent
         self.name = name
-        self.after = after
 
     def configure(self, *args, **kwargs):
         if len(args) > 0 and isinstance(args[0], type):
             args = Layer(*args, **kwargs)
-            self.parent.insert(args, after=self.after, name=self.name)
+            self.parent.append(args, name=self.name)
         else:
             self.parent.configure(self.name, *args, **kwargs)
 
+        if self.name == "normalization" and hasattr(
+            self.parent, "_configure_normalization"
+        ):
+            self.parent._configure_normalization()
+
 
 class BaseBlock(SequentialBlock):
+
+    normalization: Union[DeferredConfigurableLayer, nn.Module]
+
     def __init__(self, *args, **kwargs):
         # self.activation = DeferredConfigurableLayer(self, "activation", after="layer")
-        self.normalization = DeferredConfigurableLayer(
-            self, "normalization", after="activation"
-        )
+        self.normalization = DeferredConfigurableLayer(self, "normalization")
         super(BaseBlock, self).__init__(*args, **kwargs)
 
     def multi(self, n=1) -> Self:

deeplay/blocks/conv/conv2d.py
@@ -65,6 +65,12 @@ class Conv2dBlock(BaseBlock):
             # Assume num_features is already correct
             return self
 
+        self._configure_normalization()
+
+        return self
+
+    def _configure_normalization(self):
+
         idx = self.order.index("normalization")
         # if layer or blocks before normalization
         if any(name in self.order[:idx] for name in ["layer", "blocks"]):
@@ -72,11 +78,6 @@ class Conv2dBlock(BaseBlock):
         else:
             channels = self.in_channels
 
-        self._configure_normalization(channels)
-
-        return self
-
-    def _configure_normalization(self, channels):
         type: Type[nn.Module] = self.normalization.classtype
 
         if type == nn.BatchNorm2d:

deeplay/list.py
@@ -113,15 +113,21 @@ class LayerList(DeeplayModule, nn.ModuleList, Generic[T]):
                 # is an invalid attribute name so must be an index
                 raise
 
+            from deeplay.blocks.base import DeferredConfigurableLayer
+
             submodules = [
                 getattr(layer, name)
                 for layer in self
                 if hasattr(layer, name)
                 and (
-                    isinstance(getattr(layer, name), nn.Module)
+                    isinstance(
+                        getattr(layer, name), (nn.Module, DeferredConfigurableLayer)
+                    )
                     or inspect.ismethod(getattr(layer, name))
                 )
             ]
+
+            DeferredConfigurableLayer
 
             if len(submodules) > 0:
                 return ReferringLayerList(*submodules)
@@ -141,6 +147,9 @@ class LayerList(DeeplayModule, nn.ModuleList, Generic[T]):
             indices = list(range(len(self)))[index]
             return ReferringLayerList(*[self[idx] for idx in indices])
 
+    def __add__(self, other: "LayerList[T]") -> "ReferringLayerList[T]":
+        return ReferringLayerList(*self, *other)
+
 
 class ReferringLayerList(list, Generic[T]):
     def __init__(self, *layers: T):
@@ -152,28 +161,35 @@ class ReferringLayerList(list, Generic[T]):
         return [layer(*args, **kwargs) for layer in self]
 
     def __getattr__(self, name: str) -> "ReferringLayerList[T]":
-        try:
-            return super().__getattr__(name)
-        except AttributeError:
-            # check if name is integer string
-            if name[0] in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
-                # is an invalid attribute name so must be an index
-                raise
 
-            submodules = [
-                getattr(layer, name)
-                for layer in self
-                if hasattr(layer, name)
-                and (
-                    isinstance(getattr(layer, name), nn.Module)
-                    or inspect.ismethod(getattr(layer, name))
-                )
-            ]
+        # check if name is integer string
+        if name[0] in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
+            # is an invalid attribute name so must be an index
+            raise AttributeError(
+                f"LayerList has no attribute '{name}' in any of its layers."
+            )
 
-            if len(submodules) > 0:
-                return ReferringLayerList(*submodules)
-            else:
-                raise
+        from deeplay.blocks.base import DeferredConfigurableLayer
+
+        submodules = [
+            getattr(layer, name)
+            for layer in self
+            if hasattr(layer, name)
+            and (
+                isinstance(getattr(layer, name), (nn.Module, DeferredConfigurableLayer))
+                or inspect.ismethod(getattr(layer, name))
+            )
+        ]
+
+        if len(submodules) > 0:
+            return ReferringLayerList(*submodules)
+        else:
+            raise AttributeError(
+                f"LayerList has no attribute '{name}' in any of its layers."
+            )
+
+    def __add__(self, other: "ReferringLayerList[T]") -> "ReferringLayerList[T]":
+        return ReferringLayerList(*self, *other)
 
 
 class Sequential(LayerList, Generic[T]):