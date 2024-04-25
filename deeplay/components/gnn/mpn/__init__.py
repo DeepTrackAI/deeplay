dea7f214359a1c6988332ff57d8463364d451a7b
Jesús Pineda <jesus.pineda@physics.gu.se>
2024-04-25 09:38:29 +0200

    Update __init__.py

--- deeplay/components/gnn/mpn/__init__.py
+++ deeplay/components/gnn/mpn/__init__.py
@@ -2,7 +2,8 @@ from .mpn import MessagePassingNeuralNetwork
 from .rmpn import ResidualMessagePassingNeuralNetwork
 
 from .transformation import *
-from .propagation import Sum, Mean, Max, Min, Prod
+from .propagation import Sum, WeightedSum, Mean, Max, Min, Prod
 from .update import *
 
 from .cla import CombineLayerActivation
+from .ldw import LearnableDistancewWeighting
