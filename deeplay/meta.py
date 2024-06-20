31ba2afda499483fbe7f5619c1ecaa71e8d1ceed
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-06-20 14:23:04 +0200

    Add load from pickled checkpoint in metaclass

--- deeplay/meta.py
+++ deeplay/meta.py
@@ -1,4 +1,5 @@
 from typing import Type, TypeVar
+import pickle
 
 T = TypeVar("T")
 
@@ -16,10 +17,27 @@ class ExtendedConstructorMeta(type):
     def __call__(cls: Type[T], *args, **kwargs) -> T:
         """Construct an instance of a class whose metaclass is Meta."""
 
+        # If the object is being constructed from a checkpoint, we instead
+        # load the class from the pickled state and build it using the
+        #
+        if "__from_ckpt_application" in kwargs:
+            assert "__build_args" in kwargs, "Missing __build_args in kwargs"
+            assert "__build_kwargs" in kwargs, "Missing __build_kwargs in kwargs"
+
+            _args = kwargs.pop("__build_args")
+            _kwargs = kwargs.pop("__build_kwargs")
+
+            app = pickle.loads(kwargs["__from_ckpt_application"])
+            app.build(*_args, **_kwargs)
+            return app
+
         __user_config = kwargs.pop("__user_config", None)
         obj = cls.__new__(cls, *args, **kwargs)
         # if isinstance(obj, cls):
         cls.__pre_init__(obj, *args, **kwargs)
+
+        if __user_config:
+            obj._base_user_config = __user_config
 
         # if cls._is_top_level["value"]:
         with not_top_level(cls, obj):
