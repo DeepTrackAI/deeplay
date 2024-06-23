9136a13897fd37ffe1201254b0cddc2ad6a9df00
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-06-23 19:06:08 +0200

    refactor: Add stateful decorator for methods modifying object state

--- deeplay/decorators.py
+++ deeplay/decorators.py
@@ -2,6 +2,8 @@ from functools import wraps
 import time
 import datetime
 
+from deeplay import meta
+
 
 class Callback:
     """A deepcopy safe callback."""
@@ -22,9 +24,24 @@ class Callback:
         return id(self)
 
 
+def stateful(func):
+    """Decorator for methods modify the object state."""
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        self.root_module._append_to_tape(self, func.__name__, args, kwargs)
+
+        with self.root_module.calling_stateful():
+            func(self, *args, **kwargs)
+        return self
+
+    return wrapper
+
+
 def before_build(func):
     """Decorator for methods that will be run before build instead of immediately."""
 
+    @stateful
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         self.register_before_build_hook(Callback(func, *args, **kwargs))
@@ -39,6 +56,7 @@ def after_build(func):
     If the build method creates a new object, the hook will run on the new object.
     """
 
+    @stateful
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         self.register_after_build_hook(Callback(func, *args, **kwargs))
@@ -53,6 +71,7 @@ def after_init(func):
     If called during init, the hook will not be stored and will only run immediately.
     """
 
+    @stateful
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         func(self, *args, **kwargs)
