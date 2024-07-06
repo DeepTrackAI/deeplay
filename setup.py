549031394db580551bf7d8186e02d2ee2b578b16
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-07-07 07:44:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as file:
 
 setup(
     name="deeplay",
-    version="0.0.7",
+    version="0.1.0",
     license="MIT",
     packages=find_packages(),
     author=(
